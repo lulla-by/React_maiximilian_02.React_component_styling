5638ae85b18cda7ad747559cef0edabb2e6dae90
lulla-by <thfl3363@hanmail.net>
2023-05-02 03:04:17 +0900

    동적인 인라인 스타일 설정, 조건부 스타일링

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,7 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid,setIsValid]=useState(true)
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -12,14 +13,21 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // 입력값 유효성 체크
+    if(enteredValue.trim().length ===0){
+      // 사용자 피드백을 위한 어떠한 스타일링 필요
+      setIsValid(false)
+      return ;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/* style 속성 중 color 프로퍼티에 들어가는 값을 동적으로 설정 */}
+        <label style={{color:!isValid?"red":"black"}}>Course Goal</label>
+        <input style={{borderColor:!isValid?"red":"black"}} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
